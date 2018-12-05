52b96216e5f517f764ded159cd4f50a7b08a7f41
iamsania0895 <45395154+iamsania0895@users.noreply.github.com>
2018-12-04 19:07:14 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,79 @@
+/* =================================
+   LOADER                     
+=================================== */
+
+
+/* =================================
+===  FULL SCREEN HEADER         ====
+=================================== */
+function alturaMaxima() {
+  var altura = $(window).height();
+  $(".full-screen").css('min-height',altura); 
+  
+}
+
+$(document).ready(function() {
+  alturaMaxima();
+  $(window).bind('resize', alturaMaxima);
+});
+
+
+/* =================================
+===  SMOOTH SCROLL             ====
+=================================== */
+var scrollAnimationTime = 1200,
+    scrollAnimation = 'easeInOutExpo';
+$('a.scrollto').bind('click.smoothscroll', function (event) {
+    event.preventDefault();
+    var target = this.hash;
+    $('html, body').stop().animate({
+        'scrollTop': $(target).offset().top
+    }, scrollAnimationTime, scrollAnimation, function () {
+        window.location.hash = target;
+    });
+});
+
+
+/* =================================
+===  WOW ANIMATION             ====
+=================================== */
+wow = new WOW(
+  {
+    mobile: false
+  });
+wow.init();
+
+
+
+
+/* =================================
+===  EXPAND COLLAPSE            ====
+=================================== */
+//$('.expand-form').simpleexpand({
+   // 'defaultTarget': '.expanded-contact-form'
+//});
+
+
+
+
+
+/* =================================
+===  STELLAR                    ====
+=================================== */
+$(window).stellar({ 
+horizontalScrolling: false 
+});
+
+
+/* =================================
+===  Bootstrap Internet Explorer 10 in Windows 8 and Windows Phone 8 FIX
+=================================== */
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
